6cd974822e85037f6305443b86cbbaf6ca3ed202
Seniru <senirupasan@gmail.com>
2026-01-11 00:50:34 +0530

    feat: alerts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,35 +7,38 @@ import Index from "./pages";
 import { AuthProvider } from "./contexts/auth";
 import PrivateRoute from "./components/privateRoute";
 import AdminDashboard from "./pages/admin/dashboard";
+import { AlertProvider } from "./contexts/alert";
 
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route
-            element={
-              <div className="min-h-screen">
-                <Header />
-                <Outlet />
-              </div>
-            }
-          >
-            <Route index element={<Index />} />
-            <Route element={<PrivateRoute />}>
-              <Route path="profile" element={<Profile />} />
-            </Route>
+      <AlertProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
             <Route
-              path="admin"
-              element={<PrivateRoute requiredRoles={["admin"]} />}
+              element={
+                <div className="min-h-screen">
+                  <Header />
+                  <Outlet />
+                </div>
+              }
             >
-              <Route path="dashboard" element={<AdminDashboard />} />
+              <Route index element={<Index />} />
+              <Route element={<PrivateRoute />}>
+                <Route path="profile" element={<Profile />} />
+              </Route>
+              <Route
+                path="admin"
+                element={<PrivateRoute requiredRoles={["admin"]} />}
+              >
+                <Route path="dashboard" element={<AdminDashboard />} />
+              </Route>
             </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </AlertProvider>
     </AuthProvider>
   );
 }
---
+++ frontend/src/contexts/alert.tsx
@@ -0,0 +1,49 @@
+import { createContext, useContext, useState, type ReactNode } from "react";
+import Alert from "../components/alert";
+
+export type AlertType = "info" | "success" | "warning" | "error";
+export type AlertDirection =
+  | "top left"
+  | "top center"
+  | "top right"
+  | "middle left"
+  | "middle center"
+  | "middle right"
+  | "bottom left"
+  | "bottom center"
+  | "bottom right";
+
+export type AlertProps = {
+  message: string;
+  type: AlertType;
+  position: AlertDirection;
+};
+
+export type AlertContextProps = {
+  alertData: AlertProps | null;
+  dispatchAlert:
+    | Function
+    | React.Dispatch<React.SetStateAction<AlertProps | null>>;
+};
+
+const AlertContext = createContext<AlertContextProps>({
+  alertData: null,
+  dispatchAlert: () => {
+    console.warn("AlertContext not initialized yet");
+  },
+});
+
+export const AlertProvider = ({ children }: { children: ReactNode }) => {
+  const [alertData, dispatchAlert] = useState<AlertProps | null>(null);
+
+  return (
+    <AlertContext.Provider value={{ alertData, dispatchAlert }}>
+      <Alert alertData={alertData} dispatchAlert={dispatchAlert} />
+      {children}
+    </AlertContext.Provider>
+  );
+};
+
+export const useAlert = () => {
+  return useContext(AlertContext);
+};
